11d5d0bc609688a7559efd490eef56bc46c47e6b
Mahmoudyahya111 <113292207+Mahmoudyahya111@users.noreply.github.com>
2026-05-06 05:47:25 +0300

    Fix formatting issues in manifest.js

--- manifest.js
+++ manifest.js
@@ -1,12 +1,12 @@
 {
-  "name"; "Telegant",
-  "short_name"; "Telegant",
-  "start_url"; "index.html",
-  "display"; "standalone",
-  "background_color"; "#0d0d0d",
-  "theme_color"; "#0d0d0d",
-  "description"; "Defined by Elegance - Luxury Mock Neck Collection.",
-  "icons"; [
+  "name": "Telegant",
+  "short_name": "Telegant",
+  "start_url": "index.html",
+  "display": "standalone",
+  "background_color": "#0d0d0d",
+  "theme_color": "#0d0d0d",
+  "description": "Defined by Elegance - Luxury Mock Neck Collection.",
+  "icons": [
     {
       "src": "Tfavco-black.png",
       "sizes": "192x192",
